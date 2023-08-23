f6047eabf032718095f3ecb22dbc10a6cdcd0cc4
unknown <santosh Kumar Mohapatra>
2023-08-23 22:14:21 +0100

    first commit - api and dockerfile

--- testapi/Program.cs
+++ testapi/Program.cs
@@ -8,7 +8,7 @@ var app = builder.Build();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Chilly", "Cool",  "Warm", "Hot"
 };
 
 app.MapGet("/getweatherforecast", () =>
